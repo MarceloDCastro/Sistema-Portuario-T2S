7a640c2409c37f9e66ff8f063fb52350ceda23dd
Marcelo de Castro <marcelodecastro.ms@gmail.com>
2021-05-09 17:42:35 -0300

    Projeto criado

---
+++ src/main/java/com/t2s/sistemaportuariot2s/SistemaPortuarioT2sApplication.java
@@ -0,0 +1,13 @@
+package com.t2s.sistemaportuariot2s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPortuarioT2sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPortuarioT2sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/t2s/sistemaportuariot2s/SistemaPortuarioT2sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t2s.sistemaportuariot2s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPortuarioT2sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
